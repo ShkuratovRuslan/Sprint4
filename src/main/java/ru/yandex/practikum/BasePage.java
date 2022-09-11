fb2b8963a669624883411648a4d334c490ed5f94
Ruslan Shkuratov <shinokaze1@yandex.ru>
2022-09-11 21:04:01 +0300

    first commit

--- src/main/java/ru/yandex/practikum/BasePage.java
+++ src/main/java/ru/yandex/practikum/BasePage.java
@@ -1,11 +1,2 @@
-package ru.yandex.practikum;
-
-import org.openqa.selenium.WebDriver;
-
-public abstract class BasePage {
-    protected final WebDriver driver;
-
-    public BasePage(WebDriver driver) {
-        this.driver = driver;
-    }
+package ru.yandex.practikum;public class BasePage {
 }
